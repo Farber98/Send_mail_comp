e8cda05d8b3dde7abde14493edae39d2762d8698
Juan Farber <juantuc98@gmail.com>
2019-05-30 03:02:49 -0300

    Agregamos la logica de PHP para validar nuestros correos. Faltaria aprender a subir a un hosting para poder ejecutar la funcion mail(); y verificar el funcionamiento. Mas adelante optimizamos.

--- index.php
+++ index.php
@@ -1,7 +1,68 @@
 <?php 
 
-require 'index.view.php'; //Asociamos el archivo de la vista.
 
 
+$errores = '';  //Creamos la variable errores para luego concatenar.
+$enviado = '';  //Creamos la variable enviado para chequear que los datos fueron llenados correctamente.
 
+
+if(isset($_POST['submit']))     //Comprobamos si el formulario fue enviado. Si esta seteado es porque los datos fueron enviados por el metodo post.
+{                          //OJO: Esto no significa que relleno los datos. Significa que apreto el boton enviar correo.
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];     //Ahora si verifico que se rellenen cada uno.
+    $mensaje = $_POST['mensaje'];
+
+    if(!empty($nombre)) //Si no esta vacia
+    {
+        $nombre = trim($nombre);        //Eliminamos espacios al principio y final.
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);  //Sanamos la variable nombre, eliminamos los caracteres especiales.
+    }
+    else
+    {
+        $errores .= 'Por favor ingresa un nombre <br/>';    //Operador .= concatena el texto de variable errores que ya tenemos y le agrega lo restante que pusimos.
+    }
+
+
+    if(!empty($correo)) //Si no esta vacia
+    {
+        $correo = trim($correo);        //Eliminamos espacios al principio y final.
+        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);  //Sanamos la variable correo con FILTER_SANITIZE_EMAIL, eliminamos los caracteres especiales.
+
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL))  //Devuelve false si es un correo invalido. Sino devuelve el correo.
+        {
+            $errores .= 'Por favor ingresa un correo valido. <br/>';        //En caso de que el correo no sea valido.
+        }
+    }
+    else
+    {
+        $errores .= 'Por favor ingresa un correo <br/>';    //En caso de que el correo no haya sido ingreado.
+    }
+
+    if(!empty($mensaje))
+    {
+        $mensaje = htmlspecialchars($mensaje);      //Para que no inyecten codigo html.
+        $mensaje = trim($mensaje);              //Para eliminar espacios al principio y final.
+        $mensaje = stripslashes($mensaje);      //Elimina diagonales.
+        
+    }
+    else
+    {
+        $errores .= 'Por favor ingresa un mensaje <br/>';   //Porque esta vacio.
+    }
+
+    if(!$errores)   //Si no hay errores preparamos nuestro correo para enviar.
+    {
+        $enviar_a = 'tunombre@gmail.com';   //El correo al cual se enviaran todos los mails.
+        $asunto = 'Correo enviado desde miPagina.com';
+        $mensaje_preparado = "De: $nombre \n";
+        $mensaje_preparado .= "Correo: $correo \n";     //Preparamos el formato del mensaje.
+        $mensaje_preparado .= "Mensaje:" . $mensaje;
+
+       // mail($enviar_a, $asunto, $mensaje_preparado);       //Funcion mail recibe estos parametros. No funciona en localhost pero cuando lo subamos al hosting tendria que funcionar.
+
+        $enviado = true;    //Supongo que se pudo enviar.
+    }
+    
+}          
+require 'index.view.php';    //Asociamos el archivo de la vista. PONER AL FINAL NO SE XQ PERO SE SOLUCIONA EL ERROR DE LAS VARIABLES.                  
 ?>
--- index.view.php
+++ index.view.php
@@ -10,14 +10,21 @@
     <div class="wrap">
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">    <?php //Creamos el form segun el action que queremos (metodo post). Redirigimos al usuario a la misma pagina una vez que se envian los archivos con %SERVER['PHP_SELF']?>                                                                 
              
-             <input type="text" class="form-control" name= "nombre" id="nombre" placeholder="Nombre: " value=""> <?php //Agregamos un input tipo text para ingresar el nombre. ?>
+             <input type="text" class="form-control" name= "nombre" id="nombre" placeholder="Nombre: " value="<?php if(!$enviado && isset($nombre)) echo $nombre; //Mantenemos los valores en caso de error.?>"> <?php //Agregamos un input tipo text para ingresar el nombre. ?>
 
-             <input type="email" class="form-control" name= "correo" id="correo" placeholder="Correo: " value=""> <?php //Agregamos un input tipo email para ingresar el correo ?>
+             <input type="email" class="form-control" name= "correo" id="correo" placeholder="Correo: " value="<?php if(!$enviado && isset($correo)) echo $correo; //Mantenemos los valores en caso de error.?>"> <?php //Agregamos un input tipo email para ingresar el correo ?>
 
-             
-            <textarea name="mensaje" placeholder="Mensaje: " class="form-control" id="mensaje" > </textarea> <?php //Averiguar porque no me anda el placeholder. ?>
+            <textarea class="form-control" name="mensaje" placeholder="Mensaje: "  ><?php if(!$enviado && isset($mensaje)) echo $mensaje;   //Mantenemos los valores en caso de error. ?></textarea> <?php //Averiguar porque no me anda el placeholder. OJO CON LOS ESPACIOS QUE NO MUESTRA EL PLACEHOLDER PORQUE LO TOMA COMO TEXTO.?>
 
-           
+           <?php if(!empty($errores)): ?>   <?php //Mostramos el contenido de la variable error si no esta vacia.  ?>
+                <div class="alert error">
+                    <?php echo $errores;   ?>  <?php //Mostramos el contenido de la variable error si no esta vacia.  ?>
+                </div>
+            <?php elseif($enviado): ?>  <?php  //Variable enviado es true cuando se hayan rellenado todos los datos correctamente ?>
+                <div class="alert success">
+                    <p>Enviado correctamente.</p>
+                </div>
+            <?php endif ?>
 
             <input type="submit" name="submit" class="boton btn-primary" value="Enviar correo">
         </form>    
